9deb54c5916262cf7364e87200ca4878617288e4
rgupta2063 <36462587+rgupta2063@users.noreply.github.com>
2019-11-30 00:20:09 -0800

    Generates GroundTruth Images

---
+++ groundtruths/groundtruth.m
@@ -0,0 +1,33 @@
+i = 21;
+while (i <= 100)
+    image = rand(256,256);
+    I = imgaussfilt(image,20);
+    threshold = median(image(:));
+    BW = imbinarize(I,threshold);
+    BW = imfill(BW,'holes');
+    BW2 = -(BW-1);
+    [L2,m] = bwlabel(BW2);
+    [L,n] = bwlabel(BW);
+    if (n + m == 5)
+        L(L==0) = L2(L==0)+ max(L(:));
+        if checkRegSizes(L,900)
+            figure,imshow(L,[]);
+            save(['GT' int2str(i) '.mat'],'L');
+        else
+            continue;
+        end
+    else
+        continue;
+    end
+    i = i + 1;
+end
+
+function flag = checkRegSizes(L,n)
+    nregions = max(L(:));
+    flag = 1;
+    for i = 1:nregions
+        if sum(L==i,'all') < n
+            flag = 0;
+        end
+    end
+end
