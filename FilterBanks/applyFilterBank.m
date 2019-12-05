64842d7e3c389ad40156f8960dacf1bb14dffbeb
Basile Hurat <basile.hurat@gmail.com>
2019-12-04 20:28:57 -0800

    updated applyFilterBank

--- FilterBanks/applyFilterBank.m
+++ FilterBanks/applyFilterBank.m
@@ -25,7 +25,8 @@ ff=fft2(f);
 ewtc = zeros([size(f) (length(mfb)-1)*length(mfb{2})]);
 for s = 2:length(mfb)
     for t = 1:length(mfb{2})
-    ewtc(:,:,(s-1)*t+t) = real(ifft2(conj(mfb{s}{t}).*ff));
+    ewtc(:,:,(s-2)*length(mfb{2})+t) = real(ifft2(conj(mfb{s}{t}).*ff));
     end
 end
+
 end
