1597913c0b930237d3f69ee0beb7b60f25a6de07
Basile Hurat <basile.hurat@gmail.com>
2019-12-09 18:39:20 -0800

    added 256x256 imgs to folders 30-69

--- Outex_SS_00000/GT2TrainingData.m
+++ Outex_SS_00000/GT2TrainingData.m
@@ -1,25 +1,25 @@
 %to be run in Outex_SS_00000
 %i = 0;
- for i= 0:9
-    folname = ['00' int2str(i) '/'];
-    
-    for k = 1:5
-        begx = ceil(rand*128); begy = ceil(rand*128);
-        temp = imread([folname 'train_0' int2str(k) '.ras']);
-        textur{k} = temp;%(begy:begy+127,begx:begx+127);
-    end
-    for j = 1:250
-        load(['../groundtruths/sz256/GT' int2str(j) '.mat']);
-        im = zeros(size(L));
-        for k = 1:5
-            im(L==k) = textur{k}(L==k);
-        end
-        im = im./255;
-        imwrite(im,[folname 'sz256Data' int2str(j) '.ras'])
-    end
-    i
- end
-for i = 10:29
+%  for i= 0:9
+%     folname = ['00' int2str(i) '/'];
+%     
+%     for k = 1:5
+%         begx = ceil(rand*128); begy = ceil(rand*128);
+%         temp = imread([folname 'train_0' int2str(k) '.ras']);
+%         textur{k} = temp;%(begy:begy+127,begx:begx+127);
+%     end
+%     for j = 1:250
+%         load(['../groundtruths/sz256/GT' int2str(j) '.mat']);
+%         im = zeros(size(L));
+%         for k = 1:5
+%             im(L==k) = textur{k}(L==k);
+%         end
+%         im = im./255;
+%         imwrite(im,[folname 'sz256Data' int2str(j) '.ras'])
+%     end
+%     i
+%  end
+for i = 30:69
     folname = ['0' int2str(i) '/'];
     for k = 1:5
         begx = ceil(rand*128); begy = ceil(rand*128);
