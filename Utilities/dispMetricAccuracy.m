ffc4214f65cc9e1cb71fa93fb22ceac84be958c4
Basile Hurat <basile.hurat@gmail.com>
2019-12-13 08:33:27 -0800

    added personal nbc driver and altered metric calculator to not show names of metrics

--- Utilities/dispMetricAccuracy.m
+++ Utilities/dispMetricAccuracy.m
@@ -1,5 +1,8 @@
-function dispMetricAccuracy(groundtruths,predicted)
-measures = {
+function dispMetricAccuracy(groundtruths,predicted,verbose)
+    if nargin < 3
+        verbose = 1;
+    end
+    measures = {
             'nvoi',... % Normalized variation of information
             'ssc' ,... % Segmentation covering (two directions)
             'sdhd',... % Directional Hamming distance (two directions)
@@ -7,12 +10,20 @@ measures = {
             'vd'  ,... % Van Dongen
             'bce' ... % Bidirectional consistency error
             };
-        sumAvg = 0;
-        for ii=1:length(measures)
-    result = eval_segm(predicted, groundtruths, measures{ii});
-    disp([measures{ii} repmat(' ',1,6-length(measures{ii})) ': ' num2str(result)])
-        sumAvg = sumAvg + result;
+    sumAvg = 0;
+    for ii=1:length(measures)
+        result = eval_segm(predicted, groundtruths, measures{ii});
+        if (verbose == 1)
+            disp([measures{ii} repmat(' ',1,6-length(measures{ii})) ': ' num2str(result)])
+        else
+            disp([num2str(result)]);
         end
-        sumAvg = sumAvg/length(measures);
-    disp(['avg' '   : ' num2str(sumAvg)]);
+        sumAvg = sumAvg + result;
+	end
+    sumAvg = sumAvg/length(measures);
+    if verbose == 1
+        disp(['avg' '   : ' num2str(sumAvg)]);
+    else
+        disp(num2str(sumAvg));
+    end
 end
